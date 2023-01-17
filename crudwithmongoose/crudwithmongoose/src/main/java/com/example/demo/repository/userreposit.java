7799c580071832067319ed0dd0830b014a5f4a35
sravankomati <64322574+sravankomati@users.noreply.github.com>
2023-01-17 17:17:02 +0530

    Delete crudwithmongoose/crudwithmongoose directory

--- crudwithmongoose/crudwithmongoose/src/main/java/com/example/demo/repository/userreposit.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.userinfo;
-
-public interface userreposit extends MongoRepository<userinfo, String> {
-
-}
